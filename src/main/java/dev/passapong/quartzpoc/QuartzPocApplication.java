c6f752681223bacc5304264be0d67136887dff20
Passapong Thaithatgoon <champillon@rurouni.local>
2022-03-21 16:17:59 +0700

    initailed project

---
+++ src/main/java/dev/passapong/quartzpoc/QuartzPocApplication.java
@@ -0,0 +1,13 @@
+package dev.passapong.quartzpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/passapong/quartzpoc/QuartzPocApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.passapong.quartzpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
