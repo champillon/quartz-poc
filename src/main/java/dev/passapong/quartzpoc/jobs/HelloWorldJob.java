47ca1ccfb87e98b5948210e6d4935e22abb625b1
Passapong Thaithatgoon <champillon@rurouni.local>
2022-03-23 14:15:57 +0700

    changed hello world message to callback message

--- src/main/java/dev/passapong/quartzpoc/jobs/HelloWorldJob.java
+++ src/main/java/dev/passapong/quartzpoc/jobs/HelloWorldJob.java
@@ -1,10 +1,13 @@
 package dev.passapong.quartzpoc.jobs;
 
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import dev.passapong.quartzpoc.entities.TimerInfo;
 
 @Component
 public class HelloWorldJob implements Job {
@@ -12,7 +15,9 @@ public class HelloWorldJob implements Job {
 
     @Override
     public void execute(JobExecutionContext context) {
-        LOG.info("Hello World!!!");
+        JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
+        TimerInfo info = (TimerInfo) jobDataMap.get(HelloWorldJob.class.getSimpleName());
+        LOG.info("Received, " + info.callbackData);
     }
 
 }
